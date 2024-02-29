bbffa58c4ebe8c4ffd6915aa4b6c1a558397af2d
Mariana Fernandes <rochamarianafernandes@gmail.com>
2024-02-29 11:34:03 -0300

    Adicionando um exe e comentários

--- imc.cpp
+++ imc.cpp
@@ -4,34 +4,34 @@ using namespace std;
 
 
 int main()
-{
+{   //definido as variáveis necessárias
     float massa;
     float altura;
     double imc;
     
     do 
-    {
+    {// Requisitando as informações do usuário
      cout << "Massa corporal: "; 
      cin >> massa; 
-    }
+    } // Valores negativos serão desconsiderados
     while (massa < 0);
 
     do 
-    {
+    { // Requisitando do usuário
      cout << "Altura: "; 
      cin >> altura; 
-    }
+    } // Valores negativos serão ignorados
     while (altura < 0);
 
+    //Calculando o imc com as variáveis
     imc = massa/(altura*altura);
     
-    //std::cout << imc;
-    
+    // Definindo as categorias do imc e alertando ao usuário
     if (imc < 17)
     {
         std::cout<<"Cuidado! Muito abaixo do peso";
     }
-    
+    // Condições complementares
     else if ( imc >= 17 && imc < 18.5 )
     {
         std::cout<<"Abaixo do peso";
